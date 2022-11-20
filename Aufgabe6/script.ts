e4ce623c282feeae5489b2fad9e83402cc1b552a
LenaMarie1903 <lena.m.fleig@gmail.com>
2022-11-20 21:21:31 +0100

    Aufgabe6

---
+++ Aufgabe6/script.ts
@@ -0,0 +1,60 @@
+
+let Deutschland: number=83200000;
+console.log(Deutschland);
+let Text_Deutschland: string= "Die Einwohnerzahl von Deutschland beträgt 83200000 Millionen im Jahre 2022";
+console.log(Text_Deutschland);
+
+let Deutschland2060: number=81800000;
+console.log(Deutschland2060);
+let Text_Deutschland2060: string= "Die Einwohnerzahl von Deutschland wird betragen 81800000 Millionen im Jahre 2060";
+console.log(Text_Deutschland2060);
+
+var result:number=Deutschland-Deutschland2060;
+alert(result);
+
+
+
+
+let Frankreich: number=67800000;
+console.log(Frankreich);
+let Text_Frankreich: string= "Die Einwohnerzahl von Frankreich beträgt 67800000 Millionen im Jahre 2022";
+console.log(Text_Frankreich);
+
+let Frankreich2060: number=69700000;
+console.log(Frankreich2060);
+let Text_Frankreich2060: string= "Die Einwohnerzahl von Frankreich wird betragen 69700000 Millionen im Jahre 2060";
+console.log(Text_Frankreich2060);
+
+var result:number=Frankreich-Frankreich2060;
+alert(result);
+
+
+
+let Italien: number=59000000;
+console.log(Italien);
+let Text_Italien: string= "Die Einwohnerzahl von Italien beträgt 59000000 Millionen im Jahre 2022";
+console.log(Text_Italien);
+
+let Italien2060: number=56000000;
+console.log(Italien2060);
+let Text_Italien2060: string= "Die Einwohnerzahl von Italien wird betragen 56000000 Millionen im Jahre 2060";
+console.log(Text_Italien2060);
+
+var result:number=Italien-Italien2060;
+alert(result);
+
+
+
+let Luxenburg: number=600000;
+console.log(Luxenburg)
+let Text_Luxenburg: string= "Die Einwohnerzahl von Luxenburg beträgt 600000  im Jahre 2022";
+console.log(Text_Luxenburg);
+
+let Luxenburg2060: number=800000;
+console.log(Luxenburg2060);
+let Text_Luxenburg2060: string= "Die Einwohnerzahl von Italien wird betragen 800000  im Jahre 2060";
+console.log(Text_Luxenburg2060);
+
+var result:number=Luxenburg-Luxenburg2060;
+alert(result);
+
